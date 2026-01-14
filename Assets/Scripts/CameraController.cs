39808c15922c256a12a006abecdd682b71ca1e30
Chris Trombly <christrombly001@hotmail.com>
2026-01-14 02:01:10 -0500

    Sidequest 1C Start 2

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    public float smoothTime;
-    private Vector3 velocity = new Vector3 (2, 2, 2);
+    private float smoothTime = 2;
+    private Vector3 velocity = new Vector3 (0, 0, 2);
     Vector3 cameraStartPos;
     public PlayerController player;
 
@@ -19,7 +19,8 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         //transform.position = player.transform.position + new Vector3(0, 0, -10);
-        transform.position = Vector3.SmoothDamp(cameraStartPos, new Vector3(-10, 0, -10) ,ref velocity, smoothTime);
+        //transform.position = Vector3.SmoothDamp(cameraStartPos, new Vector3(-10, 0, -10) ,ref velocity, smoothTime);
+        Debug.Log(cameraStartPos);
     }
 
     //player.transform.position + new Vector3(0, 0, -10)
